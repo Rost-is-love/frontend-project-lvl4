e947a8ea1b9ceb3e24cd7684b924ddba723f5c38
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-05-12 17:42:51 +0300

    remove console

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,6 @@ import buildApp from '../server/index.js';
 
 test('get /', async () => {
   const app = buildApp({ port: 5000 });
-  console.log('1', app.inject);
   const response = await app.inject({
     url: '/',
   });
