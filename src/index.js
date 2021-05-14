cfc286f7307f214e2a35a4586f2fd143c2224ad2
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-05-14 12:53:12 +0300

    create rollbar

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import ReactDOM from 'react-dom';
+import Rollbar from 'rollbar';
 import { io } from 'socket.io-client';
 
 import App from './init.jsx';
 
 import '../assets/application.scss';
 
-if (process.env.NODE_ENV !== 'production') {
-  localStorage.debug = 'chat:*';
-}
-
 const runApp = async () => {
+  // eslint-disable-next-line
+  new Rollbar({
+    accessToken: 'c16a953d79474fec9f3b5f6943378688',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    localStorage.debug = 'chat:*';
+  }
+
   const chatContainer = document.querySelector('#chat');
   const socket = io();
   const vdom = await App(socket);
