66686bfa0c60996fc820adcb9036449f00ccad13
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-05-16 11:25:59 +0300

    add setTimeout for rollbar

--- src/index.js
+++ src/index.js
@@ -11,12 +11,16 @@ import App from './init.jsx';
 import '../assets/application.scss';
 
 const runApp = async () => {
+  console.log('here');
   // eslint-disable-next-line
   new Rollbar({
     accessToken: 'c16a953d79474fec9f3b5f6943378688',
     captureUncaught: true,
     captureUnhandledRejections: true,
   });
+  setTimeout(() => {
+    foo();
+  }, 1000);
 
   if (process.env.NODE_ENV !== 'production') {
     localStorage.debug = 'chat:*';
